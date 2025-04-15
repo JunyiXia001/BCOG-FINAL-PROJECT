24b2ff237e9fca80a6fae665b12d2b13f672952b
JunyiXia001 <jackxia2@illinois.edu>
2025-04-14 22:55:43 -0500

    add return self name

--- player.py
+++ player.py
@@ -18,9 +18,11 @@ class Player:
     jail_status = 0
     def __init__(self, id):
         self.id = id
-
+        self.name = f"Player {id}"
     def land_sum(self):
         return sum(land.mortage + (land.level*land.house_price)/2 for land in lands)
-
+    
+    def __str__(self):
+        return self.name
     
         
